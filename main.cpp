266d70112dfa2040e72384daddad782d4c752101
Fasih <sultanffyt09@gmail.com>
2026-08-10 13:27:39 +0500

    Practicing c++

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@ using namespace std;
 
 int main()
 {
-    cout<< "\nHello world";
-    cout<< "\nlets go";
+    cout<< "Hello world"<< '\n';
+    cout<< "lets go";
     //a comment
     /*
     a big 
-    comment*/
+    comment
+    */
+
+    int x = 5;
+    float y = 6.208;
+    double sum = x+y;
+    cout <<"\nthis is the sum "<<sum;
+    char chamba = 'c';
+    cout << '\n' <<chamba;
+
+    bool a = true;
+    bool b = false;
+    string brooski = "\nfriday hello mate God blesss you";
+    cout << "\nhello"<< brooski;
+
     return 0;
 }

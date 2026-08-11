39c3b91e465dbc6fa168a972c52ba09a338d9f14
Fasih <sultanffyt09@gmail.com>
2026-08-11 12:51:22 +0500

    Practicing C++

--- going_through_basics.cpp
+++ going_through_basics.cpp
@@ -5,15 +5,25 @@ using namespace std;
 // creating an alias for a data type
 typedef std::vector<std::pair<std::string, int>> call;
 typedef std::string ors;
-using u = int a;
+//typedef is an old C++ keyword used to create an alias(another name) for an existing keyword
+using u = int;
+// using is a keyword used to create another name for an existing data type
 int main()
 {
     ors champ = "jkfdkj";
-    cout<< "lests go " << champ;
-    int b = 3;
-    u = 4
-    cout << u;
+    cout<< "lests go " << champ << '\n';
+    u b = 20;
+    // arithmatic operations
 
+    //b=b+1;
+    //b-=1;
+    //b--;
+    //b+=1;
+    //b++;
+    //b*=2;
+    //b/=3;
+    float remainder = b%3;
+    cout << remainder;
 }
 
 

f638dd3cd926ef294a4265c13aac8e116c571f1e
Fasih <sultanffyt09@gmail.com>
2026-08-13 16:31:29 +0500

    Introduction to the Math library C++

---
+++ math_library.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    double y=3;
+    double x=1;
+    double z,t;
+    z= max(x,y);
+    t= min(x,y);
+    cout << z;
+    cout<<'\n' <<t;
+
+
+}

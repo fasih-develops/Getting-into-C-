4e04509c9b85effa74a1d8a6bc2b2734df258bd9
Fasih <sultanffyt09@gmail.com>
2026-08-13 23:53:17 +0500

    Lets go with conditional operators

---
+++ conditional_operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int x;
+    cout<< "Enter your age : ";
+    cin>> x;
+    if(x >= 18){
+        cout<< "You are eligible for this website.";
+    }
+    else if(x > 10){
+        cout<< "Get out kid";
+    }
+    else{
+        cout<< "how did you reached us bro";
+    }
+}

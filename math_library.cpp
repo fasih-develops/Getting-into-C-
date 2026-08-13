0e42f8d87401090ffdc69820327bd6c759cfe85f
Fasih <sultanffyt09@gmail.com>
2026-08-13 20:43:07 +0500

    Practicing the Math library

--- math_library.cpp
+++ math_library.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-    double y=3;
+    double y=3.5;
     double x=2;
     double z,t;
     //z= max(x,y);
@@ -12,7 +12,21 @@ int main()
     //z=pow(x,y);
     //z=sqrt(9);
     //z=abs(-2);
-    cout<<'\n' <<z;
+    //z=round(y);
+    //z=ceil(y);
+    //z=floor(y);
+    //cout<<'\n' <<z;
+    double a,b,c;
+    cout<< "Enter your number A :";
+    cin>> a;
+    cout<< "Enter your number B :";
+    cin>> b;
+    a= pow(a,2);
+    b= pow(b,2);
+    c= sqrt(a+b);
+    cout<< "Here is the side C :"<< c;
+
+    
 
 
 }

9e249aad964b7f52b42b3c468a9bb12e26136846
Fasih <sultanffyt09@gmail.com>
2026-08-13 17:46:04 +0500

    Practicing C++

--- math_library.cpp
+++ math_library.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
     double y=3;
-    double x=1;
+    double x=2;
     double z,t;
-    z= max(x,y);
-    t= min(x,y);
-    cout << z;
-    cout<<'\n' <<t;
+    //z= max(x,y);
+    //t= min(x,y);
+    //z=pow(x,y);
+    //z=sqrt(9);
+    //z=abs(-2);
+    cout<<'\n' <<z;
 
 
 }

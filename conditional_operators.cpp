48525a62c123145d07da58c7e0ac00179ca45d8b
Fasih <sultanffyt09@gmail.com>
2026-08-14 16:02:53 +0500

    Practiced switch statement

--- conditional_operators.cpp
+++ conditional_operators.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main()
 {
+    /*
     int x;
     cout<< "Enter your age : ";
     cin>> x;
@@ -47,6 +48,35 @@ int main()
     default:
         cout<< "Invalid input";
         break;
-    }
     
+    }
+    */
+    double x,y,z;
+    char opr;
+    cout<< "****************Calculator****************";
+    cout<< "\nEnter your 1st number: ";
+    cin>> x;
+    cout<< "\nEnter your second number: ";
+    cin>> y;
+    cout<<"\nEnter your operator + - * / ";
+    cin>>opr;
+
+    switch (opr){
+    case '+':
+        cout<< " Here is the sum" << x+y;
+        break;
+    case'-':
+        cout<< "Here is the difference" << x-y;
+        break;
+    case '*':
+        cout << "Here is the product" << x*y;
+        break;
+    case '/':
+        cout << "Here is the division" << x/y;
+        break;
+     default:
+        cout <<"Invalid input ";
+        break;
+     }
+    cout<< "\n****************Closing Calculator*****************"   ; 
 }

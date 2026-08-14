431520778951978f9b724435663e0ebf8e1fcc89
Fasih <sultanffyt09@gmail.com>
2026-08-14 17:26:44 +0500

    Practiced And or not operators in C++

--- conditional_statements.cpp
+++ conditional_statements.cpp
@@ -52,40 +52,21 @@ int main()
     
     }
     
-    double x,y,z;
-    char opr;
-    cout<< "****************Calculator****************";
-    cout<< "\nEnter your 1st number: ";
-    cin>> x;
-    cout<< "\nEnter your second number: ";
-    cin>> y;
-    cout<<"\nEnter your operator + - * / ";
-    cin>>opr;
-
-    switch (opr){
-    case '+':
-        cout<< " Here is the sum" << x+y;
-        break;
-    case'-':
-        cout<< "Here is the difference" << x-y;
-        break;
-    case '*':
-        cout << "Here is the product" << x*y;
-        break;
-    case '/':
-        cout << "Here is the division" << x/y;
-        break;
-     default:
-        cout <<"Invalid input ";
-        break;
-     }
-    cout<< "\n****************Closing Calculator*****************"   ; 
-    
     int x;
     cout<< "\nEnter your value ";
     cin>> x;
     x == 1 ? cout<< "its one": cout<< "its another one";
     */
-
+    
+    // and or not operators
+    int x;
+    cout<< "Enter a temperature";
+    cin>> x;
+    if(x>0 && x<30){
+        cout<< "Temperature is GoooD!";
+    }
+    else{
+        cout<< "Temperature is bad";
+    }
     
 }   

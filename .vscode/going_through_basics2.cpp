96c9fd49af1f31b28adec1a599bb117a497dea07
Fasih <sultanffyt09@gmail.com>
2026-08-16 10:03:37 +0500

    Practiced nested for loops

--- .vscode/going_through_basics2.cpp
+++ .vscode/going_through_basics2.cpp
@@ -3,13 +3,38 @@ using namespace std;
 int main()
 {
     //Nested loops
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j<=i; j++)
-        {
+   
+    for(int i=5;i>=1;i--){
+        for(int j=1; j<=i; j++){
             cout<< "*";
         }
-        cout<<"\n";
+        cout<<'\n';
     }
+
+
+    //practicing
+    /*
+    int x,y;
+    do
+    {
+        cout<< "Enter a positive number: ";
+        cin>> x;
+    } while (x<0);
+    cout<< "enter an even number: ";
+    cin>> y;
+    while (y % 2 != 0)
+    {
+        cout<< "enter an even number: ";
+        cin>> y;
+    }
+    
+    for(int i=5; i>=1; i--){
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<"*";
+        }
+        cout << endl;
+    }
+    */
     
 }

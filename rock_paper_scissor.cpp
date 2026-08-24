9d6ade3dbd8ca58348e6f42f5e103fa32750d003
Fasih <sultanffyt09@gmail.com>
2026-08-24 20:46:13 +0500

    Rock paper sciccorrrrrrrrr!!!!!1

---
+++ rock_paper_scissor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+char getuserchoice();
+char getcomputerchioce();
+void showchioce(char choice);
+void choosewinner(char player, char computer);
+
+using namespace std;
+int main()
+{
+    char player;
+    do
+    {
+        cout<<"Rock , Paper , Scissor game! \n";
+        cout<<"**************************";
+        cout<<"\n \'r\' for rock";
+        cout<<"\n \'p\' for paper";
+        cout<<"\n \'s\' for scissor";
+        cout<<"\nEnter your choice: ";
+        cin>> player;
+    } while (player != 'r' && player != 'p' && player != 's');
+    
+    
+
+}
+char getuserchoice(){
+    return 0;
+}
+char getcomputerchioce(){
+    return 0;
+}
+void showchioce(char choice){
+    return;
+}
+void choosewinner(char player, char computer){
+    return;
+}

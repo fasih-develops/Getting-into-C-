4f9e969b188e0efcc109fc1648b0becd5c4c4bff
Fasih <sultanffyt09@gmail.com>
2026-08-24 12:19:54 +0500

    Grinding on banking app and getting confused and stuck for the first time since the start of C++

--- bankingapp.cpp
+++ bankingapp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void showbalance(double balance);
@@ -8,6 +9,7 @@ double deposit();
 int main()
 {
     int choice;
+    
     double balance = 123;
     do{
         cout<<"\n**************Banking app**************";
@@ -17,13 +19,17 @@ int main()
         cout<<"\n4- Exit";
         cout<<"\n Enter your choice : ";
         cin>>choice;
+        cin.clear();
+        fflush(stdin);
         switch (choice)
         {
         case 1: showbalance(balance);
             break;
-        case 2: withdraw(balance);
+        case 2: balance -= withdraw(balance);
+            showbalance(balance);
             break;
-        case 3: deposit();
+        case 3: balance += deposit();
+            showbalance(balance);
             break;
         case 4: cout<<"Thanks for Banking Please visit us again! ";
             break;
@@ -31,14 +37,37 @@ int main()
             break;
         }
     }while(choice!= 4);
-    cout<<"**************************************";
+    cout<<"\n**************************************";
 }
 void showbalance(double balance){
-    cout<<"Your balance is $"<< balance<< '\n';
+    cout<<"Your balance is $"<<setprecision(2) <<fixed<< balance<< '\n';
 }
 double withdraw(double balance){
-    return 0;
+    double amount = 0;
+    cout<<"Enter amount to be withdrawn: ";
+    cin>>amount;
+    if (amount>balance)
+    {
+        cout<<"Insufficent balance\n";
+        return 0;
+    }
+    else
+    {
+        balance-=amount;
+        return amount;
+    }
+    
+    
 }
 double deposit(){
-    return 0;
+    double amount;
+    cout<<"Enter amount to deposit: ";
+    cin>>amount;
+    if(amount>0){
+        return amount;
+    }
+    else{
+        cout<< "You entered an invalid amount! ";
+        return 0;
+    }
 }

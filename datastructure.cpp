a65a657c2914f2e536f21bfbe33fbe801b2cc1c0
Fasih <sultanffyt09@gmail.com>
2026-08-27 13:29:16 +0500

    Used sizeof to find the n bytes of different data types and arrays

--- datastructure.cpp
+++ datastructure.cpp
@@ -6,7 +6,16 @@ int main()
     // Array is a data structure that can hold multiple values 
 
     // We declare an array and assign them strings and doubles and different data types
-    string bmws[] = {"M3","M4","M5"};
+    double num = 4;                   // 8 bytes of memory
+    char chr1 = 'a';                  // char is gets stroed into 1 byte
+
+    string chr2 = "i";                // string doesn't have a fixed byte size
+
+    int num2 = 1;                     // int 4 bytes
+    long long num3 = 2;               // long long 8 bytes
+    long double num4 = 3.14;          // long double 8-16 bytes
+    char bmws[] = {'a','b','c'};
+    
     cout<<bmws[0];
     cout<<bmws[1];
     cout<<bmws[2];
@@ -19,10 +28,10 @@ int main()
     cout<<'\n'<<numbmws[0];
     cout<<'\n'<<numbmws[1];
     cout<<'\n'<<numbmws[2];
-    cout<<'\n'<<numbmws[3];
+    cout<<'\n'<<numbmws[3]<< '\n';
      
     // The sizeof() function in C++ determines the size of a varible,datatype,class,object in bytes 
-    cout<< sizeof(numbmws[0]) << '\n'<< sizeof(numbmws[2])<< '\n'<<sizeof(numbmws[3]);
+    cout<< sizeof(bmws)/sizeof(char) << " Elements in the char array";
 
 
 }

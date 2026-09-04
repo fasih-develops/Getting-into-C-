9bdb8ffedef916d566beb291d4879743a142c0d4
Fasih <sultanffyt09@gmail.com>
2026-09-04 12:29:11 +0500

    Random dice rolling and weather generator

--- random_library.cpp
+++ random_library.cpp
@@ -8,11 +8,11 @@ int main()
     //srand =  seed random number generator , time is used to get the current time , NULL is used to just return the current time don't share it somewhere else
     //use the current time as the starting seed for random number generator
     int num1,num2,num3;
-    srand(time(NULL));
-    num1 = rand() % 7;
-    num2 = rand() % 7;
-    num3 = rand() % 7;
-    cout<< "here are nums after rolling a dice 3 times :" << "\n"<< num1 << "\n"<< num2 << "\n" << num3;
+    srand(time(0));
+    num1 = (rand() % 6)+1;
+    num2 = (rand() % 6)+1;
+    num3 = (rand() % 6)+1;
+    cout<< "here are nums after rolling a dice 3 times :" << "\n"<< num1 << "\n"<< num2 << "\n" << num3<< "\n";
     
     //time(0) = time(NULL)
     srand(time(0));

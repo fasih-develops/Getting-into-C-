c47c331f2877fa9be6be34a16e575ed98bb7fa8e
Fasih <sultanffyt09@gmail.com>
2026-09-12 11:37:31 +0500

    Luhn algorithm completed enter your card number to check

--- luhnalgorithm.cpp
+++ luhnalgorithm.cpp
@@ -12,19 +12,29 @@ int main()
     cout<<"Enter your card number: ";
     cin>>cardnumber;
     result = sumevendigit(cardnumber) + sumodddigits(cardnumber);
-
+    if(result%10 == 0){
+        cout<<"Your card number is valid!";
+    }else{
+        cout<<"Your card number is not valid!";
+    }
 
 }
 int getdigit(int const number){
-    retun number 
+    return number/10 + number%10;
 }
 int sumodddigits(string cardnumber){
-    
+    int sum = 0;
+    for(int i = cardnumber.size()-1;i>=0;i-=2){
+        sum += getdigit(cardnumber[i]-'0');
+
+    }
+    return sum;
 }
 int sumevendigit(string cardnumber){
     int sum = 0;
-    for(int i = cardnumber.size();i>=0;i-=2){
+    for(int i = cardnumber.size()-2;i>=0;i-=2){
         sum += getdigit((cardnumber[i]-'0')*2);
 
     }
+    return sum;
 }
